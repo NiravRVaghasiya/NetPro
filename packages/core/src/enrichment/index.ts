252fdb2bebed519ed75b11379ff243803c673477
NiravRVaghasiya <nirav.r.vaghasiya@gmail.com>
2026-09-01 12:25:57 +0200

    feat(core): add enrichment pipeline orchestrator with confidence-merge persistence

--- packages/core/src/enrichment/index.ts
+++ packages/core/src/enrichment/index.ts
@@ -1,1 +1,7 @@
-export const MODULE_NAME = 'enrichment';
+export * from './types';
+export * from './cache';
+export * from './rate-limiter';
+export * from './pipeline';
+export { createHunterProvider } from './providers/hunter';
+export { createPDLProvider } from './providers/pdl';
+export { createClearbitProvider } from './providers/clearbit';
